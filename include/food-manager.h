7dc43c46ef0c01ad0990a768fae48e32f7a450aa
Roman <purtov143@gmail.com>
2023-07-25 20:14:12 +0600

    [+] Update food,food-manager

--- include/food-manager.h
+++ include/food-manager.h
@@ -1,18 +1,17 @@
 #ifndef INCLUDE_FOOD_MANAGER_H_
 #define INCLUDE_FOOD_MANAGER_H_
 
-#include "../include/player.h"
 #include "../include/food.h"
+#include "../include/player.h"
 #include "vector"
 
 class FoodManager {
   const Player &player_;
   int hunger = 0;
-  std::vector<Food> food{
-      {30, 20}, {55, 15}, {70, 18}};
+  std::vector<Food> food{{30, 20}, {55, 15}, {70, 18}};
 
  public:
-  explicit FoodManager(const Player &player): player_(player) {}
+  explicit FoodManager(const Player &player) : player_(player) {}
 
   void Update();
 };
